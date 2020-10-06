13a58550be180c64872097e23938541cd526ce29
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-10-06 22:49:21 +0300

    [DEV] intro slider

--- lib/presentation/page/intro/intro.dart
+++ lib/presentation/page/intro/intro.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/presentation/component/intro/catchword.dart';
+import 'package:flutter_starter/presentation/component/intro/header.dart';
+import 'package:flutter_starter/presentation/component/intro/image_container.dart';
+import 'package:flutter_starter/presentation/component/intro/indicator.dart';
+import 'package:flutter_starter/presentation/component/intro/intro_word.dart';
+import 'package:flutter_starter/presentation/component/intro/skip.dart';
 
 class Intro extends StatefulWidget {
+	final word;
+	final image;
+	final index;
+
+	const Intro({Key key, this.word, this.image, this.index}): super(key: key);
+
   @override
-  _IntroState createState() => _IntroState();
+  _IntroState createState() => _IntroState(word, image, index);
 }
 
 class _IntroState extends State<Intro> {
+  final index;
+  final image;
+  final word;
+
+  _IntroState(this.index, this.image, this.word);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+			body: Stack(
+				children: <Widget>[
+					Skip(),
+					Header(),
+					Catchword(),
+					ImageContainer(image: image,),
+					IntroWord(word: word,),
+					Indicator(index: index),
+				],
+			);
+		);
   }
 }
--- lib/presentation/page/intro/intro_slider.dart
+++ lib/presentation/page/intro/intro_slider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/presentation/page/intro/intro.dart';
+import 'package:flutter_starter/style/images.dart';
 
 class IntroSlider extends StatefulWidget {
   @override
@@ -9,6 +11,14 @@ class IntroSlider extends StatefulWidget {
 class _IntroSliderState extends State<IntroSlider> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+			body: PageView(
+				children: <Widget>[
+					Intro(word: "introWordFirst", image: Images.introFirst, index: 1,),
+					Intro(word: "introWordSecond", image: Images.introSecond, index: 2,),
+					Intro(word: "introWordThird", image: Images.introThird, index: 3,),
+				],
+			),
+		);
   }
 }
